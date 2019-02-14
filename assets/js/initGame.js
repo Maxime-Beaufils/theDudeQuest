27f00022896390aaae76f174cf796a8147ef8bdd
Maxime-Beaufils <thelimier@gmail.com>
2019-02-14 11:11:44 +0100

    init files

---
+++ assets/js/initGame.js
@@ -0,0 +1,19 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+
+var game = new Phaser.Game(config);
---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var jeu = {
+    scene : null,
+    world : world,
+    player : player
+
+}
+
+function preload(){
+}
+function create(){
+}
+function update(){
+
+}
